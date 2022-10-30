e42589f1ee2b32571058818cb806e095e293a763
NeutronJava <116937508+NeutronJava@users.noreply.github.com>
2022-10-29 23:36:45 -0500

    Update MemeSounds.plugin.js

--- memesounds/MemeSounds.plugin.js
+++ memesounds/MemeSounds.plugin.js
@@ -94,20 +94,7 @@ module.exports = (() => {
 							let audio = new Audio("https://github.com/NeutronJava/SoundsForBetterDiscordPlugin/raw/main/memesounds/Sounds/"+sound[1].file);
 							audio.volume = this.settings.setting.volume;
 							if (this.settings.setting.SoundPitch) {
-								await Tone.start();
-  const player = new Tone.Player({
-    audio,
-    loop: false,
-    autostart: true,
-  });
-  await Tone.loaded();
-  const pitchShift = new Tone
-    .PitchShift({pitch: -8})
-    .toDestination()
-  ;
-  player.connect(pitchShift);
-  player.start();
-});
+
 								} else {
 								
 							audio.play();
