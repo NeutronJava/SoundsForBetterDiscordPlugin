227f23f7d96133b8bf89e4e4edb1b638b281e5cf
Ayeshaka <116942167+Ayeshaka@users.noreply.github.com>
2022-11-19 02:28:10 -0600

    Update MemeSounds.plugin.js

--- memesounds/MemeSounds.plugin.js
+++ memesounds/MemeSounds.plugin.js
@@ -59,7 +59,8 @@ module.exports = (() => {
 				{re: /🥶/gmi, file: "ice.mp3", duration: 600},
 				{re: /bogos binted/gmi, file: "bogosbinted.mp3", duration: 470},
 				{re: /👽/gmi, file: "fade.mp3", duration: 250},
-				{re: /bro what/gmi, file: "whatpossessedyou.mp3", duration 600}
+				{re: /bro what/gmi, file: "whatpossessedyou.mp3", duration 600},
+				{re: /,/gmi, file: "great2.mp3", duration 600}
 				
 			];
 
